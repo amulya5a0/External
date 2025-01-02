b2c413038f903c31f7304b755f2121fdf19fa0c8
amulya5a0 <147133539+amulya5a0@users.noreply.github.com>
2025-01-02 10:26:38 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,16 @@
+package CSE225a0;
+import java.util.*;
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner sc=new Scanner(System.in);
+		int n=10;
+		for(int i=0;i<=n;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
