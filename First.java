db0a21774f4db4fa994438873da0e73e6ce9e145
amulya5a0 <147133539+amulya5a0@users.noreply.github.com>
2025-01-02 10:56:15 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,10 +1,6 @@
-package CSE225a0;
 import java.util.*;
 public class First {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Scanner sc=new Scanner(System.in);
 		int n=10;
 		for(int i=0;i<=n;i++) {
 			if(i%2==0) {
